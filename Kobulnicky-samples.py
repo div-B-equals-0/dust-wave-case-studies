efa1db59b7d91c31430459275039ff29ca69e34a
William Henney <will@henney.org>
2018-10-09 13:43:11 -0500

    Calculate tau and density in ERO 21 bow

--- Kobulnicky-samples.py
+++ Kobulnicky-samples.py
@@ -6,6 +6,16 @@
 #     display_name: Python 3
 #     language: python
 #     name: python3
+#   language_info:
+#     codemirror_mode:
+#       name: ipython
+#       version: 3
+#     file_extension: .py
+#     mimetype: text/x-python
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
+#     version: 3.6.3
 # ---
 
 import numpy as np
@@ -400,6 +410,6 @@ ax.set(xscale='log', yscale='log',
       )
 ax.set_aspect('equal')
 
-# Not the same!  But 
+# Not the same!  But doesn't fully explain the $U$ scatter.  Must be different radii too
 
 
